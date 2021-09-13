a276c14d535b61dcf9130ca3a24371fac63097ce
Cathella <ncathie214@gmail.com>
2021-09-13 23:24:19 +0300

    declare app variables

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const titleInput = document.getElementById("bookTitle");
+const authorInput = document.getElementById("bookAuthor");
+const addBook = document.getElementById("add");
+const removeBook = document.getElementById("remove");
 
 function Book (id,title, author) {
     this.id = id;
@@ -8,3 +14,4 @@ function Book (id,title, author) {
 let BookArray = [new Book(1,'mustafa','cathi')];
 
 BookArray.push(new Book(2,'secound','secound'));
+
