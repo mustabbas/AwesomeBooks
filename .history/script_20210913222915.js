aa2eeb2afd2568644bf9f2c86418c4bc6e7a7d06
mustabbas <42379733+mustabbas@users.noreply.github.com>
2021-09-13 22:55:47 +0300

    Add plain Html and add list of books in array of obeject

--- .history/script_20210913222915.js
+++
@@ -1,7 +0,0 @@
-
-function Book (title, author) {
-    this.title = title;
-    this.author = author;
-  }
-
-let BookArray = [new Book('mustafa','cathi')]
--- .history/script_20210913223044.js
+++
@@ -1,9 +0,0 @@
-
-function Book (title, author) {
-    this.title = title;
-    this.author = author;
-  }
-
-let BookArray = [new Book('mustafa','cathi')];
-
-BookArray.push(new Book('secound','secound'));
