70d794b1e2af479ee5be00c0f3535646d012776d
Cathella <ncathie214@gmail.com>
2021-09-16 23:28:57 +0300

    use luxon to show date

--- script.js
+++ script.js
@@ -29,28 +29,10 @@ class Book {
   }
 
   static showDate() {
-    const current = new Date();
-    const month = [];
-    month[0] = 'January';
-    month[1] = 'February';
-    month[2] = 'March';
-    month[3] = 'April';
-    month[4] = 'May';
-    month[5] = 'June';
-    month[6] = 'July';
-    month[7] = 'August';
-    month[8] = 'September';
-    month[9] = 'October';
-    month[10] = 'November';
-    month[11] = 'December';
-
-    const cDate = `${month[current.getMonth()]} ${current.getDate()}th ${current.getFullYear()},`;
-    const cTime = `${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
-    const dateTime = `${cDate} ${cTime}`;
-
+    const { DateTime } = luxon;
     const myDate = document.querySelector('#date');
     const showMyDate = document.createElement('div');
-    showMyDate.innerHTML = `${dateTime}`;
+    showMyDate.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
     myDate.appendChild(showMyDate);
   }
 
